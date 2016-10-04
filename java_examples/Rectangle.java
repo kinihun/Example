76db9a9f0652ddc4b95703b986ec2e79598a25ff
kinihun <tokunbo@valence-technology.com>
2016-10-04 20:50:12 +0100

    Update Rectangle.java

--- java_examples/Rectangle.java
+++ java_examples/Rectangle.java
@@ -77,7 +77,7 @@ public class Rectangle {
 
     // four constructors
     public Rectangle() {
-	origin = new Point(0, 0);
+	origin = new Point(1, 1);
     }
     public Rectangle(Point p) {
 	origin = p;
